f79b031c0126456bdb18b6f4bdd78c5003be4ad6
Subbu Manickam <manickam.s@northeastern.edu>
2023-01-31 18:21:17 -0500

    Unit Test

---
+++ webapp/src/test/java/com/cloudcomputing/webapp/StatusControllerTest.java
@@ -0,0 +1,19 @@
+package com.cloudcomputing.webapp;
+
+import com.cloudcomputing.webapp.controller.StatusController;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
+
+public class StatusControllerTest {
+
+    @Test
+    public void getStatusTest() {
+        StatusController statusController = new StatusController();
+        ResponseEntity status = statusController.getAppStatus();
+        assertEquals(new ResponseEntity<>(HttpStatus.OK), status);
+    }
+}
