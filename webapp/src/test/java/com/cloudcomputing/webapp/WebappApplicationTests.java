d9fdd9465ba59a6f3680bda70e253da2a74308b3
Subbu Manickam <manickam.s@northeastern.edu>
2023-02-07 11:55:36 -0500

    GET,POST API

--- webapp/src/test/java/com/cloudcomputing/webapp/WebappApplicationTests.java
+++ webapp/src/test/java/com/cloudcomputing/webapp/WebappApplicationTests.java
@@ -2,13 +2,11 @@ package com.cloudcomputing.webapp;
 
 import com.cloudcomputing.webapp.controller.StatusController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
 class WebappApplicationTests {
 
 //	@Test
