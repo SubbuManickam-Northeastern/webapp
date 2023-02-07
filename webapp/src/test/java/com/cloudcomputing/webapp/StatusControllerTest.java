4f640bc28c690116dd06387e4033c6afeccb3cc2
Subbu Manickam <114356439+SubbuManickam@users.noreply.github.com>
2023-02-07 10:25:04 -0500

    Update StatusControllerTest.java

--- webapp/src/test/java/com/cloudcomputing/webapp/StatusControllerTest.java
+++ webapp/src/test/java/com/cloudcomputing/webapp/StatusControllerTest.java
@@ -14,6 +14,6 @@ public class StatusControllerTest {
     public void getStatusTest() {
         StatusController statusController = new StatusController();
         ResponseEntity status = statusController.getAppStatus();
-        assertEquals(new ResponseEntity<>(HttpStatus.OK), new ResponseEntity<>(HttpStatus.FORBIDDEN));
+        assertEquals(new ResponseEntity<>(HttpStatus.OK), status);
     }
 }
