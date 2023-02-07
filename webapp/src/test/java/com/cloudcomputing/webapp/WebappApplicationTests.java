e7ebeeb4ef02a720262f7a0b90c413d95372f5b2
Subbu Manickam <114356439+SubbuManickam@users.noreply.github.com>
2023-02-07 11:42:28 -0500

    Update WebappApplicationTests.java

--- webapp/src/test/java/com/cloudcomputing/webapp/WebappApplicationTests.java
+++ webapp/src/test/java/com/cloudcomputing/webapp/WebappApplicationTests.java
@@ -1,20 +1,12 @@
 package com.cloudcomputing.webapp;
 
-import com.cloudcomputing.webapp.controller.StatusController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class WebappApplicationTests {
 
 	@Test
 	void contextLoads() {
-		StatusController statusController = new StatusController();
-		ResponseEntity status = statusController.getAppStatus();
-		assertEquals(new ResponseEntity<>(HttpStatus.OK), status);
 	}
 }
